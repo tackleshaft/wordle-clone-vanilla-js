a7fc75bd595b1aeff69714d7555e9724038c7cb3
Hugh Stapleton <hstapleton808@gmail.com>
2024-03-14 14:38:40 -0400

    styling for boxes/colors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-alert('hello')
+function drawBox(container, row, col, letter='') {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = `box${row}${col}`;
+    box.textContent = letter;
+
+    container.appendChild(box);
+    return box;
+}
+
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++){
+            drawBox(grid, i, j);
+        }
+    }
+}
+
+function startUp() {
+
+}
+
+startUp();
